43b51944dd8c60f2e2ac990462ad4c5cbcd4f4f1
silence_zhpf <silence_zhpf@aliyun.com>
2021-04-12 20:36:06 +0800

    feat(test): 添加 BinarySearch的ranks静态方法

--- lib/src/main/java/search/BinarySearch.java
+++ lib/src/main/java/search/BinarySearch.java
@@ -4,13 +4,13 @@ public class BinarySearch {
     public static int rank(int key, int[] a) {
         int lo = 0;
         int hi = a.length - 1;
-        while (hi>=lo){
+        while (hi >= lo) {
             int mid = lo + (hi - lo) / 2;
             int cur = a[mid];
-            if (key==cur){
+            if (key == cur) {
                 return mid;
             }
-            if (key<cur){
+            if (key < cur) {
                 hi = mid - 1;
             }
             lo = mid + 1;
@@ -18,4 +18,26 @@ public class BinarySearch {
         return -1;
     }
 
+    public static int ranks(int key, int[] a) {
+        int lo = 0;
+        int hi = a.length - 1;
+        if (key > a[hi]) return hi + 1;
+        if (key <= a[lo]) return 0;
+        while (hi >= lo) {
+            int mid = lo + (hi - lo) / 2;
+            if (a[mid] == key && a[mid - 1] < key) {
+                return mid;
+            }
+            if (a[mid] == key && a[mid - 1] == key) {
+                hi = mid - 1;
+            }
+            if (a[mid] < key) {
+                lo = mid + 1;
+            }
+            if (a[mid] > key) {
+                hi = mid - 1;
+            }
+        }
+        return 0;
+    }
 }
--- lib/src/test/java/search/TestBinarySearch.java
+++ lib/src/test/java/search/TestBinarySearch.java
@@ -23,4 +23,32 @@ public class TestBinarySearch {
         int actual = BinarySearch.rank(key, a);
         assertEquals(actual, expected);
     }
+
+    @Test
+    public void ranks() {
+        int key = 2;
+        int[] a = {0, 0, 1, 1, 2, 2, 3, 3};
+        int expected = 4;
+        int actual = BinarySearch.ranks(key, a);
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void ranksGreatThanLast() {
+        int key = 2;
+        int[] b = {0, 0, 1, 1};
+        int expected = 4;
+        int actual1 = BinarySearch.ranks(key, b);
+        assertEquals(expected, actual1);
+    }
+
+    @Test
+    public void ranksLessThanFirst() {
+        int key = 2;
+        int[] b = {2, 2, 3, 3, 4};
+        int expected = 0;
+        int actual1 = BinarySearch.ranks(key, b);
+        assertEquals(expected, actual1);
+    }
 }
